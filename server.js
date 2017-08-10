3d4fc3d5b1409a2b6e17c53011cb0e134979ee25
yashwanth1997 <30791043+yashwanth1997@users.noreply.github.com>
2017-08-10 18:47:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,13 +16,13 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/article-one',function(req,res){
-   res.send('article one requested and served'); 
-});
+//app.get('/article-one',function(req,res){
+  // res.send('article one requested and served'); 
+//});
 app.get('/article-two',function(req,res){
    res.send('article two requested and served'); 
 });
-app.get('/article-three',function(req,res){
+app.get('/article-one',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-one.html.html'));
 });
 /*app.get('/desert.jpg', function (req, res) {
